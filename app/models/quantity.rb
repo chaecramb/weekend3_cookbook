7c630519ca02b754abd3cea1728a472b77a9e37a
Chae Cramb <chaecramb@gmail.com>
2015-11-03 09:25:13 +0000

    validation bug

--- app/models/quantity.rb
+++ app/models/quantity.rb
@@ -6,7 +6,7 @@ class Quantity < ActiveRecord::Base
   validate :unique_for_recipe
 
   def unique_for_recipe 
-    errors.add(:recipe_id, "already has a quantity for this ingredient") if Recipe.find(recipe_id).quantities.map { |q| q.ingredient_id == ingredient_id }.any?
+    #errors.add(:recipe_id, "already has a quantity for this ingredient") if Recipe.find(recipe_id).quantities.map { |q| q.ingredient_id == ingredient_id }.any?
   end
 
 end
